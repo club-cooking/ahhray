e526d2056de964c3f78e5f9dc58b6a823e91220d
ewen_ <ewenhenderson@gmail.com>
2020-09-10 20:21:52 +0100

    simplify promoters, combine all into big func

--- R/events.R
+++ R/events.R
@@ -31,11 +31,12 @@ get_event_urls <- function(region, year, month) {
 # get lineup details
 get_event_lineup <- function(page){
 
-  lineup <- page %>%
-    rvest::html_nodes("p.lineup") %>%
-    rvest::html_text()
+  lineup_element <- rvest::html_nodes(page, "p.lineup.large > a")
 
-  unlist(stringr::str_split(lineup, pattern = "\n"))
+  names <- rvest::html_text(lineup_element)
+  links <- rvest::html_attr(lineup_element, "href")
+
+  purrr::map2(names, links, ~list(artist_name = .x, artist_page = .y))
 }
 
 # get event name
@@ -46,69 +47,52 @@ get_event_name <- function(page){
     rvest::html_text()
 }
 
-# get date and venue of a event
-get_event_date_and_venue <- function(page){
+# get date of a event
+get_event_date <- function(page){
 
-  page %>%
-    rvest::html_nodes("a.cat-rev") %>%
-    rvest::html_attr("href")
+  date_element <- rvest::html_nodes(
+    page, "#detail > ul:nth-child(1) > li:nth-child(1) > a"
+    )
+
+  anytime::anydate(rvest::html_text(date_element))
 }
 
-# get promoter url
-get_promoter_url <- function(page){
+# get venue of a event
+get_event_venue <- function(page){
 
-  page %>%
-    rvest::html_nodes("div a") %>%
-    rvest::html_attr("href") %>%
-    stringr::str_subset("/promoter.aspx?")
+  venue_element <- rvest::html_nodes(
+    page, "#detail > ul:nth-child(1) > li:nth-child(2) > a.cat-rev"
+    )
+
+  names <- rvest::html_text(venue_element)
+  links <- rvest::html_attr(venue_element, "href")
+
+  purrr::map2(names, links, ~list(venue_name = .x, venue_page = .y))
 }
 
-#' get_promoter_or_club_name
-#' get a promoter or club name form their url page
-#' @param promoter_club_url
-#'
-#' @return
-#' @export
-#'
-#' @examples
-get_promoter_or_club_name <- function(promoter_club_url){
+# get promoter of a event
+get_event_promoter <- function(page){
 
-  promoter_club_name <-
-    polite_read_html(paste0("https://www.residentadvisor.net", promoter_club_url))  %>%
-    rvest::html_nodes("h1") %>%
-    rvest::html_text() %>%
-    head(1)
+  promoter_element <- rvest::html_nodes(
+    page, "#detail > ul:nth-child(1) > li:nth-child(4) > a"
+  )
 
+  names <- rvest::html_text(promoter_element)
+  links <- rvest::html_attr(promoter_element, "href")
+
+  purrr::map2(names, links, ~list(promoter_name = .x, promoter_page = .y))
 }
-
-#' find_event_promoter
-#' From en event url find the promoter name
-#' @param event_page_url
-#'
-#' @return
-#' @export
-#'
-#' get_promoter_or_club_name
-find_event_promoter <- function(page){
-
-  promoter_url <- get_promoter_url(page)
-
-  promoter_name <- get_promoter_name(promoter_url)
-
-}
-
-
-
 
 #' get_event_info
 #' Get various info from an even page url returns list
-#' @param event_page_url
+#' @param event_id numeric; event ID (taken from event URL)
 #'
-#' @return
+#' @return a list
 #' @export
 #'
-#' get_event_info(1422257)
-get_event_info <- function(event_id){
+#' @examples
+#' ra_get_event_info(event_id = 1422257)
+ra_get_event_info <- function(event_id){
 
   url <- file.path(
     "https://www.residentadvisor.net", "events", event_id
@@ -116,31 +100,17 @@ get_event_info <- function(event_id){
 
   event_page <- polite_read_html(url)
 
-  date_regex <- "([12]\\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\\d|3[01]))"
-
   event_details <- list()
 
   event_details[["event_name"]] <- get_event_name(event_page)
 
-  event_date_and_venue <- get_event_date_and_venue(event_page)
+  event_details[["event_date"]] <- get_event_date(event_page)
 
-  event_date <- anytime::anydate(stringr::str_extract(event_date_and_venue[1], date_regex))
-
-  event_details[["event_date"]] <- event_date[1]
-
-  event_details[["event_location"]] <- get_promoter_or_club_name(event_date_and_venue[2])
-
+  event_details[["venue"]] <- get_event_venue(event_page)
 
   event_details[["artist"]] <- get_event_lineup(event_page)
 
-
-  event_details[["promoter"]] <- find_event_promoter(event_page)
-
+  event_details[["promoter"]] <- get_event_promoter(event_page)
 
   event_details
-
-
 }
-
-
-
