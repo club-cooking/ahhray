2511e45af884ed3f0235ef38435ba58df36887e3
ewen_ <ewenhenderson@gmail.com>
2020-09-11 18:41:03 +0100

    tidy up event output

--- R/clubs.R
+++ R/clubs.R
@@ -11,10 +11,9 @@
 #' ra_get_clubs(ai = 10)
 #' }
 ra_get_clubs <- function(ai) {
-
   clubs <- polite_read_html(
     "https://www.residentadvisor.net/clubs.aspx",
-    query = list(ai=ai)
+    query = list(ai = ai)
   ) %>%
     rvest::html_nodes("#clubs a")
 
@@ -24,8 +23,7 @@ ra_get_clubs <- function(ai) {
   res <- list()
 
   res[["ai"]] <- ai
-  res[["clubs"]] <- lapply(seq_along(clubs), function(x){
-
+  res[["clubs"]] <- lapply(seq_along(clubs), function(x) {
     list(
       club_name = club_names[x],
       club_url = club_urls[x]
@@ -34,7 +32,3 @@ ra_get_clubs <- function(ai) {
 
   res
 }
-
-
-
-
--- R/events.R
+++ R/events.R
@@ -1,8 +1,7 @@
 
-#Seemed like the best way for this was to use the url patter, we can get country and regions
-#from this page.
+# Seemed like the best way for this was to use the url patter, we can get country and regions
+# from this page.
 get_event_regions <- function() {
-
   urls <- polite_read_html("https://www.residentadvisor.net/events?show=all") %>%
     rvest::html_nodes("div.mr8 a") %>%
     rvest::html_attr("href")
@@ -11,14 +10,13 @@ get_event_regions <- function() {
 }
 
 # get events for a region
-#Not sure how to make this simple yet but hey ho, for example lets look at london events
-#If we do it monthly, it only requires 12 calls a year?
+# Not sure how to make this simple yet but hey ho, for example lets look at london events
+# If we do it monthly, it only requires 12 calls a year?
 get_event_urls <- function(region, year, month) {
-
   url <- file.path(
     "https://www.residentadvisor.net", "events", region, "month",
     as.Date(paste(year, month, "01", sep = "-"))
-    )
+  )
 
   events <- polite_read_html(url) %>%
     rvest::html_nodes("#event-listing li a ") %>%
@@ -29,58 +27,70 @@ get_event_urls <- function(region, year, month) {
 }
 
 # get lineup details
-get_event_lineup <- function(page){
-
+get_event_lineup <- function(page) {
   lineup_element <- rvest::html_nodes(page, "p.lineup.large > a")
 
   names <- rvest::html_text(lineup_element)
-  links <- rvest::html_attr(lineup_element, "href")
+  ids <- stringr::str_extract(
+    rvest::html_attr(lineup_element, "href"),
+    "([^/]+$)"
+  )
 
-  purrr::map2(names, links, ~list(artist_name = .x, artist_page = .y))
+  purrr::map2(names, ids, ~ list(artist_name = .x, artist_id = .y))
 }
 
 # get event name
-get_event_name <- function(page){
-
+get_event_name <- function(page) {
   page %>%
     rvest::html_nodes("#sectionHead > h1") %>%
     rvest::html_text()
 }
 
 # get date of a event
-get_event_date <- function(page){
-
+get_event_date <- function(page) {
   date_element <- rvest::html_nodes(
     page, "#detail > ul:nth-child(1) > li:nth-child(1) > a"
-    )
+  )
 
   anytime::anydate(rvest::html_text(date_element))
 }
 
-# get venue of a event
-get_event_venue <- function(page){
-
+# get venue name of a event
+get_event_venue_name <- function(page) {
   venue_element <- rvest::html_nodes(
     page, "#detail > ul:nth-child(1) > li:nth-child(2) > a.cat-rev"
+  )
+
+  rvest::html_text(venue_element)
+}
+
+# get venue link of a event
+get_event_venue_id <- function(page) {
+  venue_element <- rvest::html_nodes(
+    page, "#detail > ul:nth-child(1) > li:nth-child(2) > a.cat-rev"
+  )
+
+  as.numeric(
+    stringr::str_extract(
+      rvest::html_attr(venue_element, "href"),
+      "([^=]+$)"
     )
-
-  names <- rvest::html_text(venue_element)
-  links <- rvest::html_attr(venue_element, "href")
-
-  purrr::map2(names, links, ~list(venue_name = .x, venue_page = .y))
+  )
 }
 
 # get promoter of a event
-get_event_promoter <- function(page){
-
+get_event_promoter <- function(page) {
   promoter_element <- rvest::html_nodes(
     page, "#detail > ul:nth-child(1) > li:nth-child(4) > a"
   )
 
   names <- rvest::html_text(promoter_element)
-  links <- rvest::html_attr(promoter_element, "href")
+  ids <- as.numeric(stringr::str_extract(
+    rvest::html_attr(promoter_element, "href"),
+    "([^=]+$)"
+  ))
 
-  purrr::map2(names, links, ~list(promoter_name = .x, promoter_page = .y))
+  purrr::map2(names, ids, ~ list(promoter_name = .x, promoter_id = .y))
 }
 
 #' get_event_info
@@ -92,8 +102,8 @@ get_event_promoter <- function(page){
 #'
 #' @examples
 #' ra_get_event_info(event_id = 1422257)
-ra_get_event_info <- function(event_id){
-
+#' ra_get_event_info(event_id = 1421207)
+ra_get_event_info <- function(event_id) {
   url <- file.path(
     "https://www.residentadvisor.net", "events", event_id
   )
@@ -102,15 +112,13 @@ ra_get_event_info <- function(event_id){
 
   event_details <- list()
 
+  event_details[["event_id"]] <- as.numeric(event_id)
   event_details[["event_name"]] <- get_event_name(event_page)
-
   event_details[["event_date"]] <- get_event_date(event_page)
-
-  event_details[["venue"]] <- get_event_venue(event_page)
-
-  event_details[["artist"]] <- get_event_lineup(event_page)
-
-  event_details[["promoter"]] <- get_event_promoter(event_page)
+  event_details[["venue_id"]] <- get_event_venue_id(event_page)
+  event_details[["venue_name"]] <- get_event_venue_name(event_page)
+  event_details[["artists"]] <- get_event_lineup(event_page)
+  event_details[["promoters"]] <- get_event_promoter(event_page)
 
   event_details
 }
