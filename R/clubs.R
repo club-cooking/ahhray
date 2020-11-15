ad558384b681bf2449de6b3a65a357047713a3e0
ewen_ <ewenhenderson@gmail.com>
2020-11-15 17:23:35 +0000

    simplify club extraction

--- R/clubs.R
+++ R/clubs.R
@@ -1,69 +1,3 @@
-#' Get clubs listed on RA for a region
-#'
-#' @param ai numeric; limit results to a specific region
-#' (taken from region's URL e.g. 1 for Sydney)
-#'
-#' @return a list
-#' @export
-#'
-#' @examples
-#' \dontrun{
-#' ra_get_region_clubs(ai = 10)
-#' ra_get_region_clubs(ai = 13, include_closed = TRUE)
-#' }
-ra_get_region_clubs <- function(ai, include_closed = FALSE) {
-  page <- polite_read_html(
-    "https://www.residentadvisor.net/clubs.aspx",
-    query = list(ai = ai)
-  )
-
-  clubs <- rvest::html_nodes(page, "#clubs a")
-
-  club_names <- trimws(rvest::html_text(clubs))
-  club_addresses <- rvest::html_text(
-    rvest::html_nodes(page, "#clubs .mobile-off")
-  )
-  club_ids <- as.numeric(
-    stringr::str_extract(rvest::html_attr(clubs, "href"), "([^=]+$)")
-  )
-  club_statuses <- rep("open", length(club_ids))
-
-  if (include_closed) {
-
-    closed_page <- polite_read_html(
-      "https://www.residentadvisor.net/clubs.aspx",
-      query = list(ai = ai, status = "closed")
-    )
-
-    closed_clubs <- rvest::html_nodes(closed_page, "#clubs a")
-
-    closed_club_names <- trimws(rvest::html_text(closed_clubs))
-    closed_club_addresses <- rvest::html_text(
-      rvest::html_nodes(closed_page, "#clubs .mobile-off")
-    )
-    closed_club_ids <- as.numeric(
-      stringr::str_extract(rvest::html_attr(closed_clubs, "href"), "([^=]+$)")
-    )
-    closed_club_statuses <- rep("closed", length(closed_club_ids))
-
-    club_names <- c(club_names, closed_club_names)
-    club_addresses <- c(club_addresses, closed_club_addresses)
-    club_ids <- c(club_ids, closed_club_ids)
-    club_statuses <- c(club_statuses, closed_club_statuses)
-  }
-
-  res <- list()
-
-  res[["ai"]] <- ai
-  res[["clubs"]] <- purrr::pmap(
-    list(a = club_names, b = club_addresses, c = club_ids, d = club_statuses),
-    function(a, b, c, d) list(club_name = a, club_address = b, club_id = c, club_status = d)
-  )
-
-  res
-}
-
-
 #' Get information about a club
 #'
 #' @param club_id numeric; club ID (taken from club URL)
@@ -74,6 +8,7 @@ ra_get_region_clubs <- function(ai, include_closed = FALSE) {
 #' @examples
 #' \dontrun{
 #' ra_get_club(club_id = 106730)
+#' ra_get_club(club_id = 175569)
 #' }
 ra_get_club <- function(club_id){
 
@@ -82,7 +17,7 @@ ra_get_club <- function(club_id){
   club_page <- polite_read_html(
     "https://www.residentadvisor.net/club.aspx",
     query = list(id = club_id)
-    )
+  )
 
   club_info[["club_name"]] <- club_page %>%
     rvest::html_nodes("#sectionHead span[itemprop='name']") %>%
@@ -101,4 +36,64 @@ ra_get_club <- function(club_id){
     as.numeric()
 
   club_info
+}
+
+# get features of a club
+get_club_attrs <- function(x, status = c("open", "closed")) {
+
+  status <- match.arg(status)
+
+  clubs <- rvest::html_nodes(x, "#clubs a")
+
+  club_names <- trimws(rvest::html_text(clubs))
+  club_addresses <- rvest::html_text(
+    rvest::html_nodes(x, "#clubs .mobile-off")
+  )
+  club_ids <- as.numeric(
+    stringr::str_extract(rvest::html_attr(clubs, "href"), "([^=]+$)")
+  )
+  club_statuses <- rep(status, length(club_ids))
+
+  purrr::pmap(
+    list(a = club_names, b = club_addresses, c = club_ids, d = club_statuses),
+    function(a, b, c, d) list(club_name = a, club_address = b, club_id = c, club_status = d)
+  )
+}
+
+#' Get clubs listed on RA for a region
+#'
+#' @param ai numeric; limit results to a specific region
+#' (taken from region's URL e.g. 1 for Sydney)
+#'
+#' @return a list
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' ra_get_region_clubs(ai = 13)
+#' ra_get_region_clubs(ai = 13, include_closed = TRUE)
+#' }
+ra_get_region_clubs <- function(ai, include_closed = FALSE) {
+  page <- polite_read_html(
+    "https://www.residentadvisor.net/clubs.aspx",
+    query = list(ai = ai)
+  )
+
+  # get regions clubs
+  clubs <- get_club_attrs(page)
+
+  # get closed clubs in region
+  if (include_closed) {
+
+    closed_page <- polite_read_html(
+      "https://www.residentadvisor.net/clubs.aspx",
+      query = list(ai = ai, status = "closed")
+    )
+
+    closed_clubs <- get_club_attrs(closed_page, status = "closed")
+
+    clubs <- c(clubs, closed_clubs)
+  }
+
+  list(ai = ai, clubs = clubs)
 }
